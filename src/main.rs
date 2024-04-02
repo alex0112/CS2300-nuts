cb087d75899346ae70a6a6540a73b573329cbbf2
alex <alexl@advanstaff.com>
2024-04-02 15:29:27 -0600

    initial function signatures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+
 fn main() {
     println!("Hello, world!");
 }
+
+struct Matrix;
+
+fn read_json(filename: String) -> Matrix {
+    todo!()
+}
+
+fn write_json(filename: String, m: Matrix) {
+    todo!()
+}
+
+fn matrix_union(m: Matrix, n: Matrix) -> Matrix {
+    todo!()
+}
+
+fn matrix_intersect(m: Matrix, n: Matrix) -> Matrix {
+    todo!()
+}
+
+fn matrix_compose(m: Matrix, n: Matrix) -> Matrix {
+    todo!()
+}
+
+fn matrix_square(m: Matrix, n: Matrix) -> Matrix {
+    todo!()
+}
+
+fn matrix_trans(m: Matrix, n: Matrix) -> Matrix {
+    todo!()
+}
+
